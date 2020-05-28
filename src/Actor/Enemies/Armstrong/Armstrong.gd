716296f8359276bde5ef3010e337cbe6f1c3cb87
Aidan Venter <aidandraclion892@gmail.com>
2020-05-28 12:34:59 +0200

    Add Battery UI functionality

--- src/Actor/Enemies/Armstrong/Armstrong.gd
+++ src/Actor/Enemies/Armstrong/Armstrong.gd
@@ -45,6 +45,7 @@ func handle_death():
 	if SM.state != SM.States.DEAD:
 		SM.state = SM.States.DEAD
 		Globals.GAME.num_enemies -= 1
+		Globals.GAME.BATTERY_UI.change_charge()
 		player_direction = Globals.PLAYER.SPRITE.scale.x
 		
 		velocity.x = DEATH_VECTOR.x * player_direction
